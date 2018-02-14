479a200dd36329ecce3542ea5942d97b2965537f
tatyana.shvets@itechart-group.com <tatyana.shvets@itechart-group.com>
2018-02-14 15:43:40 +0300

    Add project files.

---
+++ src/St/Controllers/MessagingController.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace St.Controllers
+{
+    public class MessagingController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View("InsertUserName");
+        }
+
+        [HttpPost]
+        public IActionResult Index(string username)
+        {
+            return View("Index", username);
+        }
+    }
+}
